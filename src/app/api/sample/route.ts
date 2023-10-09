3b98e0a8ef0c070b207a71fb5a384da5de3650b6
THUNDERSAMA <77087243+THUNDERSAMA@users.noreply.github.com>
2023-10-09 17:02:03 +0530

    sample

---
+++ src/app/api/sample/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import mysql from "mysql2/promise"
+export async function POST(request: any) {
+  try {
+    const {data} = await request.json();
+   
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password:'',
+        port: 3306,
+        database: 'sage'
+      });
+      let ar;
+      const [rows, fields] = await connection.execute('SELECT * FROM `user` WHERE STATUS=0');
+
+    const mergedResults = {
+      status: 'success',
+      results: rows,
+    };
+    return NextResponse.json(mergedResults);
+  } catch (error) {
+    //console.error("Error:", error);
+    return NextResponse.json({
+      status: 'error',
+      message: 'Request payload error',
+      errors:error,
+    });
+  }
+}
