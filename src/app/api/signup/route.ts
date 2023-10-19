4ca8788afeef37fb2aab734037d8d55a316302f1
Shambhavi Verma <shambhavi_verma28@outlook.com>
2023-10-20 02:37:37 +0530

    Signup API

---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import mysql, { RowDataPacket } from "mysql2/promise";
+import bcrypt from "bcrypt";
+export async function POST(request:any) {
+    try{
+        const { name, email, password }= await request.json();
+
+        const connection = await mysql.createConnection({
+            host: 'sql12.freemysqlhosting.net',
+            user: 'sql12654081',
+            password:'eAD7GlKj5H',
+            port: 3306, 
+            database: 'sql12654081'
+            });
+            const [existingUser] = await connection.execute<RowDataPacket[]>('SELECT * FROM `user` WHERE email = ?', [email])
+            if(existingUser.length>0){
+              return NextResponse.json({
+                status: 'error',
+                message: 'Email already in use'
+              })
+            }
+
+            const hashedPassword= await bcrypt.hash(password, 10);
+            let ar;
+            const [rows] = await connection.execute('INSERT INTO `user` (NAME, EMAIL, PASSWORD) VALUES ("'+ name +'","'+ email +'","'+ hashedPassword +'")');
+           // [name, email, password, confirmpassword]
+          const mergedResults = {
+            status: 'success',
+            results: rows,
+          };
+          return NextResponse.json({ message: "User Registered" }, { status: 201 });
+         
+        } catch (error) {
+          return NextResponse.json({
+            status: 'error',
+            message: 'Request payload error',
+            errors:error,
+          });
+    }   
+}
